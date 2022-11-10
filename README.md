63039f0f24f9e2dcefc7d0cae73d2b73ccfb7ed9
Abigail Queen <107533064+Abi-Queen@users.noreply.github.com>
2022-11-10 06:25:50 -0800

    drafted User model

--- README.md
+++ README.md
@@ -13,7 +13,8 @@ Here is the [GitHub repo](https://github.com/Abi-Queen/social-network-API).
 This project was created by [Abigail Queen](https://github.com/Abi-Queen.git).
 
 ## Other resources utilized:
-- [UC Davis Coding Bootcamp 2022](https://bootcamp.ucdavis.edu/), Module 18
+- [UC Davis Coding Bootcamp 2022](https://bootcamp.ucdavis.edu/): Module 18 and the email validation regex from the Challenge 17 assignment
+- An article on how to validate email field input in Mongoose schema from [folkstalk.com](https://www.folkstalk.com/2022/09/mongoose-schema-email-type-with-code-examples.html)
 
 ## Date
 Created in November 2022.
---
+++ models/user.js
@@ -0,0 +1,57 @@
+// require mongoose
+const { Schema, model } = require('mongoose');
+
+// require dateFormat
+const dateFormat = require('../utils/dateFormat');
+
+// validate email function
+var validateEmail = function(email) {
+    var re = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/
+    return re.test(email)
+}
+
+// define User schema
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      validate: [validateEmail, 'Please provide a valid email address'],
+      match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, 'Please provide a valid email address.']
+    },
+    thoughts: [],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+      }
+    ]
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    id: false
+  }
+)
+
+// get total count of comments and replies on retrieval
+UserSchema.virtual('friendCount').get(function() {
+  return this.friends.reduce(
+    (total, friends) => total + friends.length + 1,
+    0
+  )
+})
+
+// export User model
+const User = model('User', UserSchema);
+module.exports = User;
